918be7649f72d4b82eeea24f585b2a54359b757d
I337592 <parisanoushazaran@gmail.com>
2025-07-26 17:40:23 +0100

    add error boundary

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { TaskProvider } from './TaskContext';
+import ErrorBoundary from './ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TaskProvider>
-      <App />
-    </TaskProvider>
+    <ErrorBoundary>
+      <TaskProvider>
+        <App />
+      </TaskProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
